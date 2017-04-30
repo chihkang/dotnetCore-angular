fdb9fdadfd65e039d5612cfddc72d744c65e564c
ChihKang <chihkang@me.com>
2017-04-30 23:25:17 +0800

    Extending the Domain Model

--- Controllers/FeatureController.cs
+++ Controllers/FeatureController.cs
@@ -20,7 +20,7 @@ namespace vega.Controllers
             this.context = context;
 
         }
-        [HttpGet("/api/vehicles/features")]
+        [HttpGet("/api/features")]
         public async Task<IEnumerable<FeatureResource>> GetFeatures()
         {
             var features = await context.Features.ToListAsync();
