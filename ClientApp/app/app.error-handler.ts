7dc3f531590be39e54b4ef40d35613c3f3ce3e82
ChihKang <chihkang@me.com>
2017-05-10 21:11:12 +0800

    Development vs Production Environment

--- ClientApp/app/app.error-handler.ts
+++ ClientApp/app/app.error-handler.ts
@@ -1,15 +1,20 @@
 import * as Raven from 'raven-js';
 import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler, Inject, NgZone } from "@angular/core";
+import { ErrorHandler, Inject, NgZone, isDevMode } from "@angular/core";
 
 export class AppErrorHandler implements ErrorHandler {
     constructor(
         private ngZone: NgZone,
         @Inject(ToastyService) private toastyService: ToastyService) {
     }
-    handleError(error: any): void {
-        Raven.captureException(error.originalError || error);
 
+    handleError(error: any): void {
+
+        if(!isDevMode())
+            Raven.captureException(error.originalError || error);
+        else
+            throw error;
+            
         this.ngZone.run(() => {
             this.toastyService.error({
                 title: 'Error',
