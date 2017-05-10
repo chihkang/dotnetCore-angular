aa384d8714286572a7630a0b024df60fe76b1c2a
ChihKang <chihkang@me.com>
2017-05-10 17:40:08 +0800

    Logging Errors

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,3 +1,4 @@
+import * as Raven from 'raven-js';
 import { AppErrorHandler } from './app.error-handler';
 import { ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -14,6 +15,10 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+
+Raven
+    .config('https://5831931939d44e3d94ca87082f034a01@sentry.io/166809')
+    .install();
 
 @NgModule({
     bootstrap: [ AppComponent ],
