ca512d44c087c41c4b9f2c799945b57516b68b3a
ChihKang <chihkang@me.com>
2017-05-21 22:26:37 +0800

    Rendering the Uploaded Photos

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,34 +16,43 @@ namespace vega.Controllers
 {
     // /api/vehicles/1/photos
     [Route("/api/vehicles/{vehicleId}/photos")]
-    public class PhothsController : Controller
+    public class PhotosController : Controller
     {
         private readonly IHostingEnvironment host;
-        private readonly IVehicleRepository repository;
+        private readonly IVehicleRepository vehicleRepository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
         private readonly PhotoSettings photoSettings;
-        public PhothsController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper, IOptionsSnapshot<PhotoSettings> options)
+        private readonly IPhotoRepository photoRepository;
+        public PhotosController(IHostingEnvironment host, IVehicleRepository vehicleRepository, IPhotoRepository photoRepository, IUnitOfWork unitOfWork, IMapper mapper, IOptionsSnapshot<PhotoSettings> options)
         {
+            this.photoRepository = photoRepository;
             this.photoSettings = options.Value;
             this.mapper = mapper;
             this.unitOfWork = unitOfWork;
-            this.repository = repository;
+            this.vehicleRepository = vehicleRepository;
             this.host = host;
 
+        }
+        [HttpGet]
+        public async Task<IEnumerable<PhotoResource>> GetPhotos(int vehicleId)
+        {
+            var photos = await photoRepository.GetPhotos(vehicleId);
+
+            return mapper.Map<IEnumerable<Photo>, IEnumerable<PhotoResource>>(photos);
         }
         [HttpPost]
         public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
         {
-            var vehicle = await repository.GetVehicle(vehicleId, includeRelated: false);
+            var vehicle = await vehicleRepository.GetVehicle(vehicleId, includeRelated: false);
 
             if (vehicle == null)
                 return NotFound();
-            
-            if(file == null) return BadRequest("Null file");
-            if(file.Length == 0) return BadRequest("Empty file");
-            if(file.Length > photoSettings.MaxBytes) return BadRequest("Max file size exceeded");
-            if(!photoSettings.IsSupported(file.FileName)) return BadRequest("Invalid file type.");
+
+            if (file == null) return BadRequest("Null file");
+            if (file.Length == 0) return BadRequest("Empty file");
+            if (file.Length > photoSettings.MaxBytes) return BadRequest("Max file size exceeded");
+            if (!photoSettings.IsSupported(file.FileName)) return BadRequest("Invalid file type.");
 
             var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
             if (!Directory.Exists(uploadsFolderPath))
@@ -61,7 +71,7 @@ namespace vega.Controllers
 
             await unitOfWork.CompleteAsync();
 
-            return Ok(mapper.Map<Photo,PhotoResource>(photo));
+            return Ok(mapper.Map<Photo, PhotoResource>(photo));
         }
     }
 }
