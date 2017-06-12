5c19196d4eb5608501fde6fa9f4db5262a80309e
ChihKang <chihkang@me.com>
2017-06-12 22:04:01 +0800

    Securing APIs

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using vega.Controllers.Resources;
@@ -24,6 +25,7 @@ namespace vega.Controllers
 
         }
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> CreateVehicle([FromBody]SaveVehicleResource vehicleResource)
         {        
             if (!ModelState.IsValid)
@@ -59,6 +61,7 @@ namespace vega.Controllers
         }
 
         [HttpPut("{id}")] // /api/vehicles/{id}
+        [Authorize]
         public async Task<IActionResult> UpdateVehicle(int id, [FromBody]SaveVehicleResource vehicleResource)
         {
             if (!ModelState.IsValid)
@@ -83,6 +86,7 @@ namespace vega.Controllers
         }
 
         [HttpDelete("{id}")]
+        [Authorize]
         public async Task<IActionResult> DeleteVehicle(int id)
         {
             var vehicle = await repository.GetVehicle(id, includeRelated: false);
--- Startup.cs
+++ Startup.cs
@@ -64,6 +64,14 @@ namespace WebApplicationBasic
 
             app.UseStaticFiles();
 
+            var options = new JwtBearerOptions
+            {
+                Audience = "https://api.vega.com",
+                Authority = "https://chihkang.auth0.com/"
+            };
+            app.UseJwtBearerAuthentication(options);
+
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
